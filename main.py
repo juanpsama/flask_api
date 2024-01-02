ded97befccffd8dd884a839f75dd0afa108997ef
JpgDev <samajpablo@gmail.com>
2024-01-01 21:30:50 -0600

    feat: make get a post operations in products and categories

--- main.py
+++ main.py
@@ -1,28 +1,89 @@
-from flask import Flask
+from flask import Flask, request
 from flask.json import jsonify
 from flask_sqlalchemy import SQLAlchemy
 
-from models import User, Product, Categories, db
+from models import User, Product, Category, db
 
 app = Flask(__name__)
+
+# Make that jsonify fuction DON'T order key pairs 
+app.json.sort_keys = False
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cake_store.db'
 db.init_app(app)
 
 
 with app.app_context():
-    
+
     #If you define models in other modules, you must import them before calling create_all, otherwise SQLAlchemy will not know about them.
     db.create_all()
 
 
-@app.get('/')
-def get_data():
-    return jsonify({'message' : 'Hola desde get'})
+@app.post('/categories')
+def post_categories():
+    request_data = request.get_json()
+    try:
+        new_category = Category(
+            title = request_data['title']
+        )
+    except:
 
-@app.post('/')
+        # If nothing was submitted as the title in request_data['title']
+        return jsonify({'error': 'data not found in request'}), 401
+    
+    db.session.add(new_category)
+    db.session.commit()
+
+    return jsonify({'message' : 'Category created succesfuly'}), 201
+
+@app.get('/categories')
+def get_categories():
+    result = db.session.execute(db.select(Category))
+    categories = result.scalars().all()
+
+    # Creates a list with all objects transform in to dictionaries
+    results_list = [category.to_dict() for category in categories]
+    
+    return jsonify(results_list)
+
+# CRUD products
+@app.get('/products')
+def get_all_products():
+    result = db.session.execute(db.select(Product))
+    products = result.scalars().all()
+
+    # Creates a list with all objects transform in to dictionaries
+    results_list = [product.to_dict() for product in products]
+    
+    return jsonify(results_list)
+
+@app.get('/products/<int:product_id>')
+def get_product(product_id):
+    try:
+        product = db.get_or_404(Product, product_id)
+    except:
+        return jsonify({"error" : "product not found with that id"}), 404
+    
+    return jsonify(product.to_dict())
+
+@app.post('/products')
 def post_data():
-    return jsonify({'message' : 'Hola desde post'})
+    request_data = request.get_json()
+    try:
+        new_product = Product(
+            title = request_data['title'],
+            description = request_data['description'],
+            category_id = request_data['category_id']
+        )
+    except:
+
+        # If some data is missing to construct the product model in the request
+        return jsonify({'error': 'data not found in request'}), 401
+    
+    db.session.add(new_product)
+    db.session.commit()
+
+    return jsonify({'message' : 'product created succesfully'}), 201
 
 @app.delete('/')
 def delete_data():
--- models.py
+++ models.py
@@ -28,13 +28,32 @@ class Product(db.Model):
     title = db.Column(db.String(100), unique = True)
     description = db.Column(db.Text)
     
-    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"))
-    category = relationship('Categories', back_populates= 'product')
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id"))
+    category = relationship('Category', back_populates= 'product')
+
+    def to_dict(self):
+        dict_result = {}
+        for column in self.__table__.columns:
+            if column.name == 'category_id':
+                
+                #Replacing the id with the total info in category 
+                dict_result['category'] = self.category.to_dict()
+            else:
+                dict_result[column.name] =  getattr(self, column.name)
+        return dict_result
+        print(self.category.to_dict())
+        # Method 2. Altenatively use Dictionary Comprehension to do the same thing.
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
 
 
-class Categories(db.Model):
-    __tablename__ = 'categories'
+
+class Category(db.Model):
+    __tablename__ = 'category'
     id = db.Column(db.Integer, primary_key=True) 
     title = db.Column(db.String(100), unique = True)
 
     product = relationship('Product', back_populates = 'category') 
+
+    def to_dict(self):
+        # Method 2. Altenatively use Dictionary Comprehension to do the same thing.
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
